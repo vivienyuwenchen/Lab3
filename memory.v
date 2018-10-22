68020c3c41a86183d25091f069f4ab313a29ee96
vivienyuwenchen <vivienyuwenchen@gmail.com>
2018-10-22 19:36:17 -0400

    Add instructions/decoder skeleton, memory

---
+++ memory.v
@@ -0,0 +1,24 @@
+//------------------------------------------------------------------------
+// Instruction Memory
+//------------------------------------------------------------------------
+
+module memory
+(
+  input clk, regWE,
+  input[9:0] Addr,
+  input[31:0] DataIn,
+  output[31:0]  DataOut
+);
+
+  reg [31:0] mem[1023:0];
+
+  always @(posedge clk) begin
+    if (regWE) begin
+      mem[Addr] <= DataIn;
+    end
+  end
+
+  initial $readmemh(“file.dat”, mem);
+
+  assign DataOut = mem[Addr];
+endmodule
